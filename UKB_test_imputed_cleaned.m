08ca32b9e98260a22fd809221333afbb200b3b46
Oleksandr Frei <oleksandr.frei@gmail.com>
2019-06-27 16:01:11 +0200

    cleanup

--- UKB_test_imputed_cleaned.m
+++ UKB_test_imputed_cleaned.m
@@ -1,22 +1,36 @@
 % download and compile https://se.mathworks.com/matlabcentral/fileexchange/27076-shuffle  (just once)
 mex Shuffle.c 
 
+bfile = '/home/oleksanf/vmshare/data/UKBDATA/genotypes/imputed/UKB26502_QCed_230519';
+LDmat_file = '/home/oleksanf/vmshare/data/UKBDATA/genotypes/imputed/UKB26502_QCed_230519_r2p1.mat';
+roi_file = '/home/oleksanf/vmshare/data/UKBDATA/phenotypes/FS/UKB26502_GWAS_qnorm_ecdf_ordered_230519.txt'
+dict_file = '/home/oleksanf/vmshare/data/UKBDATA/phenotypes/FS/FSdict_ver53_230519.txt';
+out_prefix = '/home/oleksanf/vmshare/github/most/results/lamreg0';
+
+do_prune = true;   % only use LD idependent SNPs
+maf_thresh = 0.05; % only use SNPs with maf above this threshold
+lam_reg = 0;        % regularization threshold for pre-whitening
+                    % 0 means "100% whitening", 1 disables whitening
+chr_filter = 1;
+
 snps = 10408776; nsubj = 26502;
-df = readtable('/home/oleksanf/vmshare/data/UKBDATA/phenotypes/FS/UKB26502_GWAS_qnorm_ecdf_ordered_230519.txt');
-fname = '/home/oleksanf/vmshare/data/UKBDATA/genotypes/imputed/UKB26502_QCed_230519';
-ymat = table2array(df);  %d.Properties.VariableNames - column names
-ymat_orig = ymat;
+df = readtable(roi_file);
+ymat_orig = table2array(df);  %d.Properties.VariableNames - column names
+npheno=size(ymat_orig, 2);
 rois = df.Properties.VariableNames;
 
 % in the data dictionary, preppend 'x' to 3rd_Ventricle, 4th_Ventricle, 5th_Ventricle, because that's what MATLAB does to column names in df table
-df_dict = readtable('/home/oleksanf/vmshare/data/UKBDATA/phenotypes/FS/FSdict_ver53_230519.txt', 'Delimiter', 'tab');
+df_dict = readtable(dict_file, 'Delimiter', 'tab');
 for i=1:length(df_dict.ROI_name)
   if ismember(df_dict.ROI_name{i}(1), '0123456789'), df_dict.ROI_name{i} = sprintf('x%s', df_dict.ROI_name{i}); end
 end
 
 % Sparse binary matrix for random pruning, LD r2 of .1
-load('/home/oleksanf/vmshare/data/UKBDATA/genotypes/imputed/UKB26502_QCed_230519_r2p1.mat'); % LDmat, chrnumvec, posvec, mafvec
-defvec = randn(size(LDmat, 1), 1); defvec(mafvec < 0.05) = nan; defvec = FastPrune(defvec, LDmat);
+if ~exist('LDmat', 'var'), load(LDmat_file); end; % LDmat, chrnumvec, posvec, mafvec
+defvec = randn(size(LDmat, 1), 1);
+if isfinite(maf_thresh), defvec(mafvec < maf_thresh) = nan; end;
+if isfinite(chr_filter), defvec(chrnumvec ~= chr_filter) = nan; end;
+if do_prune, defvec = FastPrune(defvec, LDmat); end;
 snp_idx = find(isfinite(defvec));
 
 mask = false(length(rois), 1);
@@ -31,51 +45,41 @@ for i=1:length(rois),
   table.is_ASEG(i, 1)         = strcmp('SubcorticalVolume', subset.ROI_type{1}); 
   table.measure{end+1, 1} = rois{i};
 end
+table.type = zeros(length(rois), 1);
+table.type(table.is_DK_AREA) = 1;
+table.type(table.is_DK_VOLUME) = 2;
+table.type(table.is_DK_THICKNESS) = 3;
+table.type(table.is_ASEG) = 4;
 
-data = {};
+if 1
+  ymat = nan(size(ymat_orig));
+  for domain = unique(table.type)'
+    C = corr(ymat_orig(:, table.type == domain));
+    C_reg = (1-lam_reg)*C + lam_reg*diag(max(0.01,diag(C))); % Ridge regularized covariance matrix
+    C_inv = inv(C_reg);
+    W_wht = chol(C_inv); % Whitening filter
+    ymat(:, table.type == domain) = ymat_orig(:, table.type == domain)*W_wht'; % Whitened residualized data
+  end
 
-if 0
-  npheno=size(ymat, 2);
   zmat=zeros(length(snp_idx), npheno, 2, 'single'); 
 
-  lamvec = 0:0.2:1; 
-  for lami=1:length(lamvec)
-
-  lam_reg = lamvec(lami);
-  data{lami}.lam_reg = lam_reg;
-
-  chunk = 300000;
+  chunk = 10000;
   for i=1:chunk:length(snp_idx)
     j=min(i+chunk-1, length(snp_idx));
     fprintf('loading snps %i to %i... ', i, j);    tic;
-    %geno_int8 = PlinkRead_binary2(nsubj, snp_idx(i:j), fname);
-    %fprintf('processing... ', i, j);   
-    %geno = nan(size(geno_int8), 'single'); for code = int8([0,1,2]), geno(geno_int8==code) = single(code); end;
+    geno_int8 = PlinkRead_binary2(nsubj, snp_idx(i:j), bfile);
+    fprintf('processing... ', i, j);   
+    geno = nan(size(geno_int8), 'single'); for code = int8([0,1,2]), geno(geno_int8==code) = single(code); end;
 
-    if 0
-      C = corr(ymat_orig);
-    
-      C_reg = (1-lam_reg)*C + lam_reg*diag(max(0.01,diag(C))); % Ridge regularized covariance matrix
-      C_inv = inv(C_reg);
-      W_wht = chol(C_inv); % Whitening filter
-      ymat = ymat_orig*W_wht'; % Whitened residualized data
-
-    end
-
-    tic
     [~, zmat_orig] = nancorr(ymat, geno);
     [~, zmat_perm] = nancorr(ymat, Shuffle(geno));
     zmat(i:j, :, 1) = zmat_orig';
     zmat(i:j, :, 2) = zmat_perm';
     fprintf('done in %.1f sec, %.1f %% completed\n', toc, 100*(j+1)/length(snp_idx));
   end
-  data{lami}.zmat = zmat;
-  data{lami}.ymat = ymat;
-
-  %zmat_bak = zmat;
-  end
-
-  save('gwas2.UKB26502_GWAS_qnorm_ecdf_ordered_230519.mat', '-v7.3', 'zmat');
+  zmat_bak = zmat;
+  
+  %save('gwas2.UKB26502_GWAS_qnorm_ecdf_ordered_230519.mat', '-v7.3', 'zmat');
 else
   load('gwas.UKB26502_GWAS_qnorm_ecdf_ordered_230519.mat');
 end
@@ -88,7 +92,8 @@ ivec_list = {{'dk_area',       find(table.is_DK_AREA)}, ...
              {'dk_all',        find(table.is_DK_AREA | table.is_DK_THICKNESS | table.is_DK_VOLUME)}, ...
              {'aseg_vol',      find(table.is_ASEG)}, ...
              {'all',           find(table.is_DK_AREA | table.is_DK_THICKNESS | table.is_ASEG)}};
-%ivec_list = {{'aseg_vol',      find(table.is_ASEG)}};
+%ivec_list = {{'all',           find(table.is_DK_AREA | table.is_DK_THICKNESS | table.is_ASEG)}};
+
 for iveci = 1:length(ivec_list)
   ivec = ivec_list{iveci}{2}; ivec_name = ivec_list{iveci}{1};
   fprintf('%s: ', ivec_name);
@@ -98,9 +103,9 @@ for iveci = 1:length(ivec_list)
   fprintf('... (%i in total)\n', length(ivec));
 end
 
-fh1 = sfigure(1001); clf; 
-fh2 = sfigure(1002); clf; 
-fh3 = sfigure(1003); clf; 
+fh1 = figure('units','normalized','outerposition',[0 0 1 1]);
+fh2 = figure('units','normalized','outerposition',[0 0 1 1]);
+fh3 = figure('units','normalized','outerposition',[0 0 1 1]);
 for iveci = 1:length(ivec_list)
 
   ivec = ivec_list{iveci}{2}; ivec_name = ivec_list{iveci}{1};
@@ -121,10 +126,11 @@ for iveci = 1:length(ivec_list)
 %  C0_reg = diag(diag(C0)); % Complete regularization -- results in imperfect gamma fit
 %  C0_reg = eye(size(C0)); % Complete regularization -- results in imperfect gamma fit
 %  max_lambda = s(min(10, length(s)));
-  max_lambda = min(0.1, s(min(10, length(s)))); % oleksanf: don't regularize unless it's too bad
+%  max_lambda = min(0.1, s(min(10, length(s)))); % oleksanf: don't regularize unless it's too bad
 
-  C0_reg = U*diag(max(max_lambda,s))*U'; % Good gamma fit
+ % C0_reg = U*diag(max(max_lambda,s))*U'; % Good gamma fit
 %  C0_reg = U*diag(max(s(40),s))*U';
+   C0_reg = C0;  % no regularization
 
   logpdfvecs = NaN(size(zmat,3),size(zmat,1)); minpvecs = NaN(size(zmat,3),size(zmat,1)); maxlogpvecs = NaN(size(zmat,3),size(zmat,1));
   for i  = 1:size(zmat,3)
@@ -165,20 +171,23 @@ for iveci = 1:length(ivec_list)
   minp_prunevec = maxlogpvecs_corr(1,ivec_snp_good); minp_prunevec(is_low_maf) = nan; minp_prunevec(minp_prunevec < -log10(5e-8)) = nan; 
   most_prunevec = logpdfvecs_corr(1,ivec_snp_good); most_prunevec(is_low_maf) = nan; most_prunevec(most_prunevec < -log10(5e-8)) = nan; 
   
-  fprintf('\n');
-  fprintf(1,'%s: GWAS yield                       minP: %d; MOST: %d\n',ivec_name,sum(maxlogpvecs_corr(1,ivec_snp_good)>-log10(5e-8)),sum(logpdfvecs_corr(1,ivec_snp_good)>-log10(5e-8)));
-  fprintf(1,'%s: GWAS yield, maf > 0.05           minP: %d; MOST: %d\n',ivec_name,sum(isfinite(minp_prunevec)),sum(isfinite(most_prunevec)));
+  fid = fopen(sprintf('%s.log', out_prefix), 'a');
+  fprintf(fid, '\n');
+  fprintf(fid,'%s: GWAS yield                       minP: %d; MOST: %d\n',ivec_name,sum(maxlogpvecs_corr(1,ivec_snp_good)>-log10(5e-8)),sum(logpdfvecs_corr(1,ivec_snp_good)>-log10(5e-8)));
+  fprintf(fid,'%s: GWAS yield, maf > 0.05           minP: %d; MOST: %d\n',ivec_name,sum(isfinite(minp_prunevec)),sum(isfinite(most_prunevec)));
 
   minp_prunevec = FastPrune(minp_prunevec, LDmat); most_prunevec = FastPrune(most_prunevec, LDmat);
-  fprintf(1,'%s: GWAS yield, maf > 0.05, prunning minP: %d; MOST: %d\n',ivec_name,sum(isfinite(minp_prunevec)),sum(isfinite(most_prunevec)));
-  fprintf(1,'%s: #measures: %i; #eff(minP): %.1f; #eff(MOST)=%.1f;\n',ivec_name, length(ivec), pd_minpvecs.b, pd_logpdfvecs.a);
+  fprintf(fid,'%s: GWAS yield, maf > 0.05, prunning minP: %d; MOST: %d\n',ivec_name,sum(isfinite(minp_prunevec)),sum(isfinite(most_prunevec)));
+  fprintf(fid,'%s: #measures: %i; #eff(minP): %.1f; #eff(MOST)=%.1f;\n',ivec_name, length(ivec), pd_minpvecs.b, pd_logpdfvecs.a);
+  fclose(fid);
 
   minPval = maxlogpvecs_corr(1, :);
   mostPval = logpdfvecs_corr(1, :);
-  save(sprintf('MOST_minP_%s.mat', ivec_name), '-v7', 'mostPval', 'minPval');
+  save(sprintf('%s_%s.mat', out_prefix, ivec_name), '-v7', 'mostPval', 'minPval');
+  
   drawnow;
 end
 
-saveas(fh1,'fh1.pdf')
-saveas(fh2,'fh2.pdf')
-saveas(fh3,'fh3.pdf')
+saveas(fh1,sprintf('%s_fh1.pdf', out_prefix))
+saveas(fh2,sprintf('%s_fh2.pdf', out_prefix))
+saveas(fh3,sprintf('%s_fh3.pdf', out_prefix))
