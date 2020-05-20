fc32ed61ba0278bbcb86e0ab3cee09e16d4b88cd
interCM <inter.cm@outlook.com>
2020-05-20 13:30:11 +0200

    Separate scripts for generation of zmat and downstream analysis.

---
+++ most_stats.m
@@ -0,0 +1,114 @@
+% =============== parameters section =============== 
+
+% Required args:
+%   zmat_names: array of file names with univariate GWAS results from the MOSTest analysis
+%   out:        output file prefix
+if ~exist('zmat_names_array', 'var'), error('zmat files array is required'); end              
+if ~exist('out', 'var'), error('out file prefix is required'); end
+
+% Optional args:
+% num_eigval_to_keep:   how many largest eigenvalues of C0 matrix (z score correlation)
+%                       to keep, the remaining will be assigned to the num_eigval_to_keep-th
+%                       eigenvalue, num_eigval_to_keep = 0 - keep all
+% use_paretotails:      fit tail of the mostest and minp statistics with pareto
+% paretotails_quantile: a number close to 1.0, used as a second argument in MATLAB's paretotails
+if ~exist('num_eigval_to_keep', 'var'), num_eigval_to_keep = 0; end
+if ~exist('use_paretotails', 'var'), use_paretotails = false; end
+if ~exist('paretotails_quantile', 'var'), paretotails_quantile = 0.99; end
+% =============== end of parameters section ===============
+
+tic
+
+n_zmat = numel(zmat_names_array);
+fprintf('%d zmat files will be procesed', n_zmat);
+
+% combine z-score mtrixes
+mostvecs = NaN(2,0); minpvecs = NaN(2,0); maxlogpvecs = NaN(2,0);
+ivec_snp_good = false(0);
+for i_zmat = 1:n_zmat
+    zmat_name = zmat_names_array[i_zmat]
+    fprintf('loading %s... ', zmat_name);
+    load(zmat_name);
+    fprintf('OK.\n')
+    nsnps = size(zmat_orig, 1);
+    npheno = size(zmat_orig, 2);
+
+    fprintf('estimating MOSTest and MinP statistics...')
+
+    ivec_snp_good_i = all(isfinite(zmat_orig) & isfinite(zmat_perm), 2);
+    % estimate correlation matrix and regularize only for the first iteration
+    % This is not 100% accurate but should be accurate enough.
+    if i_zmat == 1
+        
+        % use correlation structure of the z scores, calculated under permutation
+        % we don't weight SNPs by LD because the permutation scheme breaks the LD structure
+        % correlation structure of the null z scores
+        C0 = corr(zmat_perm(ivec_snp_good_i, :));
+        C1 = corr(zmat_orig(ivec_snp_good_i, :));
+        [U S]  = svd(C0); s = diag(S);
+
+        if (num_eigval_to_keep == 0) max_lambda = 0; else max_lambda = s(num_eigval_to_keep); end
+        C0_reg = U*diag(max(max_lambda,s))*U'; % Good gamma fit
+    end
+
+    mostvecs_i = NaN(2,nsnps); minpvecs_i = NaN(2,nsnps); maxlogpvecs_i = NaN(2,snps);
+    for i  = 1:2
+      if i==1, zmat=zmat_orig; else zmat=zmat_perm; end;
+      mostvecs_i(i,:) = dot(inv(C0_reg)*zmat', zmat');
+      minpvecs_i(i,:) = 2*normcdf(-max(abs(zmat), [], 2));
+      maxlogpvecs_i(i, :) = -log10(minpvecs_i(i, :));
+    end
+    mostvecs = [mostvecs, mostvecs_i];
+    minpvecs = [minpvecs, minpvecs_i];
+    maxlogpvecs = [maxlogpvecs, maxlogpvecs_i];
+    ivec_snp_good = [ivec_snp_good, ivec_snp_good_i]
+end
+
+[hc_maxlogpvecs hv_maxlogpvecs] = hist(maxlogpvecs(2,ivec_snp_good),1000); chc_maxlogpvecs = cumsum(hc_maxlogpvecs)/sum(hc_maxlogpvecs);
+[hc_mostvecs hv_mostvecs] = hist(mostvecs(2,ivec_snp_good),1000); chc_mostvecs = cumsum(hc_mostvecs)/sum(hc_mostvecs);
+
+if use_paretotails
+  pd_maxlogpvecs = paretotails(maxlogpvecs(2,ivec_snp_good), 0.0, paretotails_quantile);
+  pd_minpvecs_params = upperparams(pd_maxlogpvecs);
+  cdf_minpvecs = pd_maxlogpvecs.cdf(hv_maxlogpvecs);
+  maxlogpvecs_corr = -log10(pd_maxlogpvecs.cdf(maxlogpvecs,'upper'));
+
+  pd_mostvecs = paretotails(mostvecs(2,ivec_snp_good),  0.0, paretotails_quantile);
+  pd_mostvecs_params = upperparams(pd_mostvecs);
+else
+  pd_minpvecs = fitdist(colvec(minpvecs(2,ivec_snp_good)),'beta'); % Not a great fit
+  pd_minpvecs_params = [pd_minpvecs.a, pd_minpvecs.b];
+  cdf_minpvecs=cdf(pd_minpvecs,10.^-hv_maxlogpvecs,'upper');
+  maxlogpvecs_corr = -log10(cdf(pd_minpvecs,minpvecs));
+
+  pd_mostvecs = fitdist(colvec(mostvecs(2,ivec_snp_good)),'gamma'); % Seems to work -- beta and wbl  do not
+  pd_mostvecs_params = [pd_mostvecs.a, pd_mostvecs.b];
+end
+
+cdf_mostvecs = pd_mostvecs.cdf(hv_mostvecs);
+mostvecs_corr = -log10(cdf(pd_mostvecs,mostvecs,'upper'));
+fprintf('Done.\n')
+
+fprintf('GWAS yield minP: %d; MOST: %d\n',sum(maxlogpvecs_corr(1,ivec_snp_good)>-log10(5e-8)),sum(mostvecs_corr(1,ivec_snp_good)>-log10(5e-8)));
+fprintf('%i\t%.2f\t%.3f\t%.3f\t%.3f\t%.3f\t\n', npheno, cond(C0), pd_minpvecs_params(1), pd_minpvecs_params(2), pd_mostvecs_params(1), pd_mostvecs_params(2)) 
+
+most_time_sec = toc;
+
+minp_log10pval_orig = maxlogpvecs_corr(1, :);
+most_log10pval_orig = mostvecs_corr(1, :);
+minp_log10pval_perm = maxlogpvecs_corr(2, :);
+most_log10pval_perm = mostvecs_corr(2, :);
+fname=sprintf('%s.mat', out);
+fprintf('saving %s... ', fname);
+save(fname, '-v7', ...
+ 'most_log10pval_orig', 'minp_log10pval_orig', ...
+ 'most_log10pval_perm', 'minp_log10pval_perm', ...
+ 'nvec', 'freqvec', 'ivec_snp_good', ...
+ 'measures', 'ymat_corr', 'C0', 'C1', ...
+ 'minpvecs', 'mostvecs', ...
+ 'hv_maxlogpvecs', 'hc_maxlogpvecs', 'chc_maxlogpvecs', 'cdf_minpvecs', ...
+ 'hv_mostvecs', 'hc_mostvecs', 'chc_mostvecs', 'cdf_mostvecs', ...
+ 'pd_minpvecs_params', 'pd_mostvecs_params', 'most_time_sec');
+fprintf('Done.\n')
+
+fprintf('MOSTest analysis is completed.\n')
---
+++ most_zmat.m
@@ -0,0 +1,135 @@
+% =============== parameters section =============== 
+
+% Required args:
+%   out:   output file prefix
+%   pheno: file with phenotyes
+%   bfile: plink bfile prefix 
+if ~exist('out', 'var'),   error('out file prefix is required'); end
+if ~exist('pheno', 'var'), error('pheno file is required'); end
+if ~exist('bfile', 'var'), error('bfile is required'); end
+
+% Optional args:
+%   chunk:                chunk size (how many SNPs to read at a time)
+%   apply_int:            apply rank-based inverse normal transform
+%   auto_compile_shuffle: automatically compile shuffle.mex
+%   perform_cca:          perform canonical correlation analysis
+%   lam_reg:              strength of pre-whitening filter, default is to disable
+if ~exist('chunk', 'var'), chunk = 10000; end
+if ~exist('apply_int', 'var'), apply_int = true; end
+if ~exist('auto_compile_shuffle', 'var'), auto_compile_shuffle = 1; end
+if ~exist('perform_cca', 'var'), perform_cca = false; end
+if ~exist('lam_reg', 'var'), lam_reg = 1.0; end
+      
+% =============== end of parameters section =============== 
+
+if auto_compile_shuffle && (exist('Shuffle') ~= 3), mex 'Shuffle.c'; end;   % ensure Shuffle is compiled
+
+tic
+
+if isempty(zmat_name)
+
+fileID = fopen(sprintf('%s.bim', bfile));
+bim_file = textscan(fileID,'%s %s %s %s %s %s');
+fclose(fileID);
+if isfinite(snps) && (snps ~= length(bim_file{1})), error('snps=%i is incompatible with .bim file; please check your snps parameter (or remove it to auto-detect #snps)', snps);end
+snps=length(bim_file{1});
+
+fileID = fopen(sprintf('%s.fam', bfile));
+fam_file = textscan(fileID,'%s %s %s %s %s %s');
+fclose(fileID);
+if isfinite(nsubj) && (nsubj ~= length(fam_file{1})), error('nsubj=%i is incompatible with .fam file; please check your snps parameter (or remove it to auto-detect nsubj)', nsubj);end
+nsubj=length(fam_file{1});
+
+fprintf('%i snps and %i subjects detected in bfile\n', snps, nsubj);
+
+fprintf('Loading phenotype matrix from %s... ', pheno);
+if 1 
+    ymat_df = readtable(pheno, 'Delimiter', 'tab');
+    measures = ymat_df.Properties.VariableNames;
+    ymat_orig = table2array(ymat_df);
+else
+    % an alternative helper code that reads phenotype matrix without a header
+    ymat_orig=dlmread(pheno); ymat_orig=ymat_orig(:, 2:end);
+    measures = cell(size(ymat_orig, 2), 1);
+    for i=1:length(measures), measures{i} = sprintf('V%i', i); end;
+end
+npheno=size(ymat_orig, 2);
+fprintf('Done, %i phenotypes found\n', npheno);
+if size(ymat_orig, 1) ~= nsubj, error('roi matrix has info for %i subjects, while nsubj argument is specified as %i. These must be consistent.', size(ymat_orig, 1), nsubj); end;
+
+keep = (min(ymat_orig)~=max(ymat_orig));
+fprintf('Remove %i phenotypes (no variation)\n', length(keep) - sum(keep));
+ymat_orig = ymat_orig(:, keep);
+measures = measures(keep);
+npheno=size(ymat_orig, 2);
+
+% perform rank-based inverse normal transform, equivalently to the following R code:
+% DM[,f] <- qnorm(ecdf(DM[,f])(DM[,f]) - 0.5/dim(DM)[1])
+kurt = nan(npheno, 2);
+for pheno_index=1:npheno
+  vals = ymat_orig(:, pheno_index); kurt(pheno_index, 1) = kurtosis(vals);
+  if apply_int
+    [F, X] = ecdf(vals); F=transpose(F(2:end)); X=transpose(X(2:end));
+    vals = norminv(interp1(X,F,vals,'nearest') - 0.5 / length(vals));
+  end
+  ymat_orig(:, pheno_index) = vals; kurt(pheno_index, 2) = kurtosis(vals);
+end
+fprintf('kurtosis before INT - %.2f %.2f (mean, max)\n', mean(kurt(:, 1)), max(kurt(:, 1)))
+if apply_int, fprintf('kurtosis after  INT - %.2f %.2f (mean, max)\n', mean(kurt(:, 2)), max(kurt(:, 2))); end;
+
+C = corr(ymat_orig);
+C_reg = (1-lam_reg)*C + lam_reg*diag(max(0.01,diag(C))); % Ridge regularized covariance matrix
+C_inv = inv(C_reg);
+W_wht = chol(C_inv); % Whitening filter
+ymat = ymat_orig*W_wht'; % Whitened residualized data
+
+ymat_corr = corr(ymat);
+
+fprintf('Perform GWAS on %s (%i SNPs are expected)...\n', bfile, snps)
+zmat_orig=zeros(snps, npheno, 'single');
+zmat_perm=zeros(snps, npheno, 'single');
+beta_orig=zeros(snps, npheno, 'single');  % skip saving p-values and standard errors (SE)
+beta_perm=zeros(snps, npheno, 'single');  % (can be derived from Z and BETA)
+nvec=zeros(snps, 1, 'single');
+freqvec=zeros(snps, 1, 'single');
+zvec_cca=nan(snps, 2);
+
+for i=1:chunk:snps
+  j=min(i+chunk-1, snps);
+  fprintf('gwas: loading snps %i to %i... ', i, j);    tic;
+  geno_int8 = PlinkRead_binary2(nsubj, i:j, bfile);
+  fprintf('processing... ', i, j);   
+  geno = nan(size(geno_int8), 'single'); for code = int8([0,1,2]), geno(geno_int8==code) = single(code); end;
+
+  shuffle_geno = Shuffle(geno);
+  [rmat_orig_chunk, zmat_orig_chunk] = nancorr(ymat, geno);
+  [rmat_perm_chunk, zmat_perm_chunk] = nancorr(ymat, shuffle_geno);
+
+  if perform_cca
+    fprintf('cca... ');   
+    ymat1 = [ymat, ones(size(ymat, 1), 1)];
+    for k=i:j
+      % These two are equivalent:
+      % [b, bint, r, rint, stats] = regress(y, [X ones(n, 1)]);  stats(3)      % based on F-test
+      % [A, B, r, U, V, statsCCA] = canoncorr(X, y);             statsCCA.p  
+      [b, bint, r, rint, stats] = regress(geno(:,         k-i+1), ymat1); zvec_cca(k, 1) = stats(3);
+      [b, bint, r, rint, stats] = regress(shuffle_geno(:, k-i+1), ymat1); zvec_cca(k, 2) = stats(3);
+    end
+  end
+
+  zmat_orig(i:j, :) = zmat_orig_chunk';
+  zmat_perm(i:j, :) = zmat_perm_chunk';
+  beta_factor = std(ymat)' * (1./std(geno, 'omitnan'));
+  beta_orig(i:j, :) = transpose(rmat_orig_chunk .* beta_factor);
+  beta_perm(i:j, :) = transpose(rmat_perm_chunk .* beta_factor);
+  nvec(i:j) = sum(isfinite(geno))';
+  freqvec(i:j) = (1*sum(geno==1) + 2*sum(geno==2))' ./ (2*nvec(i:j));
+  fprintf('done in %.1f sec, %.1f %% completed\n', toc, 100*(j+1)/snps);
+end
+
+gwas_time_sec = toc;
+
+fname = sprintf('%s_zmat.mat', out);
+fprintf('saving %s as -v7.3... ', fname);
+save(fname, '-v7.3', 'zmat_orig', 'zmat_perm', 'beta_orig', 'beta_perm', 'measures', 'nvec', 'zvec_cca', 'freqvec', 'ymat_corr', 'gwas_time_sec');
+fprintf('OK.\n')
