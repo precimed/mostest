6232078809ebfa9b894075426acbec7d2e48e3f6
interCM <inter.cm@outlook.com>
2020-05-31 00:32:26 +0200

    Change variable name: pval -> pval_vec

--- clump.m
+++ clump.m
@@ -1,8 +1,8 @@
-function survive = clump(bfile, pval, pval_thresh, r2_thresh)
+function survive = clump(bfile, pval_vec, pval_thresh, r2_thresh)
 
 % Args:
 %   bfile:       plink bfile prefix (N SNPs and M samples).
-%   pval:        array of p-values, size(pval) = N, the order of SNPs must correspond to the order in bfile.
+%   pval_vec:    array of p-values, size(pval_vec) = N, the order of SNPs must correspond to the order in bfile.
 %   pval_thresh: p-value threshold for clumping.
 %   r2_thresh:   r2 threshold for clumping
 % Return:
@@ -14,7 +14,7 @@ fam_ids = textscan(fam_file_id,'%s %*[^\n]');
 fclose(fam_file_id);
 n_samples=length(fam_ids{1});
 
-survive = pval < pval_thresh; % == 0 for NaN values
+survive = pval_vec < pval_thresh; % == 0 for NaN values
 idx_to_clump = find(survive);
 
 geno_int8 = PlinkRead_binary2(n_samples, idx_to_clump, bfile);
@@ -22,7 +22,7 @@ geno = single(geno_int8);
 geno(geno < 0) = nan;
 geno_r2_mat = nancorr(geno, geno).^2;
 
-[sorted_pval, sorted_i] = sort(pval(idx_to_clump));
+[sorted_pval, sorted_i] = sort(pval_vec(idx_to_clump));
 clump_order = idx_to_clump(sorted_i);
 
 for i = 1:numel(clump_order)
