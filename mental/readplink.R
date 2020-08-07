99fdd0bfa6f714f88b53645332c07131b4e9414a
interCM <inter.cm@outlook.com>
2020-08-07 23:35:56 +0200

    Minor code formatting.

--- mental/readplink.R
+++ mental/readplink.R
@@ -16,7 +16,7 @@ get_bed_geno = function(bfile, isnp, nsamples, byte_geno_map=NULL) {
     #   genomat = get_bed_geno('/path/to/chr1',1:100,10000)
 
     if( is.null(byte_geno_map) ) {
-            byte_geno_map = get_byte_geno_map()
+        byte_geno_map = get_byte_geno_map()
     }
 
     bed_path = paste(bfile, 'bed', sep='.')
@@ -25,20 +25,20 @@ get_bed_geno = function(bfile, isnp, nsamples, byte_geno_map=NULL) {
     # check magic bytes
     magic_bytes = readBin(bedf, 'integer', n=3, size=1, signed=FALSE)
     if( !all( magic_bytes == c(108, 27, 1)) ) {
-            stop('Not a valid Plink BED file')
+        stop('Not a valid Plink BED file')
     }
 
     genomat = matrix(0, nrow=nsamples, ncol=length(isnp))
     nbytes = if( nsamples%%4 > 0 ) nsamples%/%4 + 1 else nsamples%/%4
     j = 1
     for( i in isnp ) {
-            seek(bedf, 3 + (i-1)*nbytes)
+        seek(bedf, 3 + (i-1)*nbytes)
         bvec = readBin(bedf, 'integer', n=nbytes, size=1, signed=FALSE)
-            geno_i = byte_geno_map[,bvec + 1]
-            geno_i = matrix(geno_i, nrow=4*nbytes, ncol=1) # reshape to column vector
-                geno_i = geno_i[1:nsamples]
-                genomat[,j] = geno_i
-                    j = j + 1
+        geno_i = byte_geno_map[,bvec + 1]
+        geno_i = matrix(geno_i, nrow=4*nbytes, ncol=1) # reshape to column vector
+        geno_i = geno_i[1:nsamples]
+        genomat[,j] = geno_i
+        j = j + 1
     }
 
     close(bedf)
@@ -56,7 +56,7 @@ get_byte_geno_map = function() {
     bit_shifts = c(0, 2, 4, 6)
     byte_geno_map = matrix(0, nrow=4, ncol=256)
     for( b in 0:255 ) {
-            byte_geno_map[,b+1] = geno_codes[bitwAnd(bitwShiftR(b, bit_shifts), 3) + 1]
+        byte_geno_map[,b+1] = geno_codes[bitwAnd(bitwShiftR(b, bit_shifts), 3) + 1]
     }
     return(byte_geno_map)
 }
