323a1df7ab8fbb97beeb9396482fd40f6594da40
Aihua Lin <61153831+aihualin@users.noreply.github.com>
2021-10-14 15:46:36 +0200

    Update step2_effective_sample_size.m

--- step2_effective_sample_size.m
+++ step2_effective_sample_size.m
@@ -2,7 +2,7 @@ clear all;
 close all;
 clc;
 
-n_cohort=250;
+n_cohort=45;
 n_sample=randi([50 70],1,n_cohort);
 n_sample_t=sum(n_sample);
 index_cohort=cumsum(n_sample);
@@ -15,16 +15,16 @@ eps=rand(n_sample_t,1);
 y_t=x_t*beta+eps;
 rat=var(x_t*beta)/var(y_t);
 
-cov=0:2000:index_cohort(end);%sort(randperm(55,n_cov));% number of covariates
+cov=0:200:index_cohort(end);%sort(randperm(55,n_cov));% number of covariates
 n_cov=size(cov,2)
 for i=1:n_cohort 
     x_i=x_t(1:index_cohort(i));
     y_i=y_t(1:index_cohort(i));
     y_i=y_i/norm(y_i);% normalize
     for j=1:n_cov
-        mc=normrnd(mu,sigma,[index_cohort(i),j]);
+        mc=normrnd(mu,sigma,[index_cohort(i),cov(j)]);
         % generate effect size of covariates
-        r=linspace(0.1,0.2,j)';
+        r=linspace(0.1,0.2,cov(j))';
         beta_es=mc\y_i;%(y-eps);
         y_res=y_i-mc*beta_es; % residulize
         
@@ -35,14 +35,15 @@ z2=t.*t;
   
 figure
 for j1=1:n_cov
-    plot(index_cohort-cov(j1),z2(:,j1),'LineWidth',2)
+    plot(index_cohort,z2(:,j1),'LineWidth',2)
     %plot(index_cohort,z2(:,j1),'LineWidth',2)
     legendInfo{j1} = ['No.cov= ' num2str(cov(j1))]; 
     hold on
 end
 xlabel('Sample size')
 ylabel('Z^2')
-%xlim([-index_cohort(end) index_cohort(end)])
+xlim([-index_cohort(end) index_cohort(end)])
 xlim([0 index_cohort(end)])
 legend(legendInfo,'Position',[0.7 0.2 0.1 0.2])
+
 
